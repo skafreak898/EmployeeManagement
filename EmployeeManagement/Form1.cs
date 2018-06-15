261a0a840261698385f89791ba99dac75eaf6b8d
skafreak898 <skafreak898@yahoo.com>
2018-06-14 20:27:34 -0500

    Removed RB events

--- EmployeeManagement/Form1.cs
+++ EmployeeManagement/Form1.cs
@@ -32,59 +32,21 @@ namespace EmployeeManagement
         
         private void rbShow3_CheckedChanged(object sender, EventArgs e)
         {
-            /*
-            if (rbShow3.Checked == true)
-            {
-                // For loop to get row count and hide all rows
-               for (int i = 0; i < dataGridView1.Rows.Count; i++)
-                {
-                    // *Might work to fix bug* dataGridView1.Rows[i].Visible = true;
-                    dataGridView1.Rows[i].Visible = false;
-
-                    // Iterate through rows and make first 3 visible
-                    for (int j = 0; j < 3; j++)
-                    {
-                        dataGridView1.Rows[j].Visible = true;
-                    }
-                }
-            }*/
+            
         }
         
 
         
         private void rbShow5_CheckedChanged(object sender, EventArgs e)
         {
-            /*
-            if (rbShow5.Checked == true)
-            {
-                // For loop to get row count and hide all rows
-                for (int i = 0; i < dataGridView1.Rows.Count; i++)
-                {
-                    // *Might work to fix bug* dataGridView1.Rows[i].Visible = true;
-                    dataGridView1.Rows[i].Visible = false;
-
-                    // Iterate through rows and make first 5 visible
-                    for (int j = 0; j < 5; j++)
-                    {
-                        dataGridView1.Rows[j].Visible = true;
-                    }
-                }
-            }*/
+            
         }
         
 
         
         private void rbShowAll_CheckedChanged(object sender, EventArgs e)
         {
-            /*
-            if (rbShowAll.Checked == true)
-            {
-                // For loop to get row count and make all rows visible.
-                for (int i = 0; i < dataGridView1.Rows.Count; i++)
-                {
-                    dataGridView1.Rows[i].Visible = true;
-                }
-            }*/
+            
         }
         
 
